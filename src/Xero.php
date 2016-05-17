e059afcb5adf7cb69709f23770b66dcea1e9d3a9
Peter Coles <peter@kuroiweb.com>
2016-05-17 19:39:11 +0100

    Class property and doc blocks

--- src/Xero.php
+++ src/Xero.php
@@ -4,11 +4,27 @@ namespace PeterColes\Xero;
 
 class Xero
 {
+    /**
+     * The credentials needed to authenticate an API call
+     */
+    protected $config;
+
+    /**
+     * Constructor
+     * 
+     * @param array $config
+     */
     public function __construct($config)
     {
         $this->config = $config;
     }
 
+    /**
+     * Magic method to direct requests to the appropriate API class
+     *
+     * @param string $method
+     * @param array  $params
+     */
     public function __call($method, $params)
     {
         $class = 'PeterColes\\Xero\\Api\\'.ucfirst($method);
